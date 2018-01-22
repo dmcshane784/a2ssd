9d1a218f26179f057854827b2ca82be8a75a332f
dmcshane784 <dmcshane784@c2ken.net>
2018-01-22 22:22:03 +0000

    Shutdown entered

--- a2ssdqub/a2ssdqub/Program.cs
+++ a2ssdqub/a2ssdqub/Program.cs
@@ -17,6 +17,7 @@ namespace a2ssdqub
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Exit();
         }
     }
 }
