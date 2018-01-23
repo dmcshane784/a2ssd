833168bb133ac3ff3a892c3e52e68cbed3020dd2
Conail Stewart <conail@users.noreply.github.com>
2018-01-22 20:33:42 -0700

    Use model in DAL and add All() method.

--- a2ssdqub/a2ssdqub/DAL/ContactDAL.cs
+++ a2ssdqub/a2ssdqub/DAL/ContactDAL.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using a2ssdqub.Models;
 
 namespace a2ssdqub.DAL
 {
@@ -12,16 +13,40 @@ namespace a2ssdqub.DAL
     {
         private static string connectionString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
 
-        public static int Add(string forename, string surname)
+        public static int Add(Contact contact)
         {
             using (var handle = new SqlConnection(connectionString))
             {
                 handle.Open();
-                var cmd = new SqlCommand(string.Format("INSERT INTO Contacts (Forename, Surname) VALUES ('{0}', '{1}')", forename, surname), handle);
+                var query = string.Format("INSERT INTO Contacts (Forename, Surname) VALUES ('{0}', '{1}')", contact.Forename, contact.Surname);
+                var cmd = new SqlCommand(query, handle);
                 int rows = cmd.ExecuteNonQuery();
                 handle.Close();
                 return rows;
             }
+        }
+
+        public static List<Contact> All()
+        {
+            var list = new List<Contact>();
+
+            using (var handle = new SqlConnection(connectionString))
+            {
+                handle.Open();
+
+                var cmd = new SqlCommand("SELECT * FROM Contacts", handle);
+                var reader = cmd.ExecuteReader();
+
+                while(reader.Read()) list.Add(new Contact {
+                    Id = (int) reader["Id"],
+                    Forename = (string) reader["Forename"],
+                    Surname = (string) reader["Surname"]
+                });
+
+                handle.Close();
+            }
+
+            return list;
         }
     }
 }
