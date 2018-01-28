7645c73e158b478c74c68d93191e8f7209c0b322
dmcshane784 <dmcshane784@c2ken.net>
2018-01-28 01:48:52 +0000

    ADD EDIT DELETE HAPPINESS

--- a2ssdqub/a2ssdqub/UpdateCustomer.Designer.cs
+++ a2ssdqub/a2ssdqub/UpdateCustomer.Designer.cs
@@ -47,7 +47,7 @@
             this.radUnknown = new System.Windows.Forms.RadioButton();
             this.dtpDob = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.panChoose.SuspendLayout();
             this.panForm.SuspendLayout();
             this.SuspendLayout();
@@ -90,7 +90,7 @@
             // 
             // panForm
             // 
-            this.panForm.Controls.Add(this.button2);
+            this.panForm.Controls.Add(this.btnUpdate);
             this.panForm.Controls.Add(this.dtpDob);
             this.panForm.Controls.Add(this.radUnknown);
             this.panForm.Controls.Add(this.radUndisclosed);
@@ -235,15 +235,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnUpdate
             // 
-            this.button2.Location = new System.Drawing.Point(229, 188);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(202, 41);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "UPDATE NOW";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnUpdate.Location = new System.Drawing.Point(229, 188);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(202, 41);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "UPDATE NOW";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button2_Click);
             // 
             // UpdateCustomer
             // 
@@ -289,6 +289,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
